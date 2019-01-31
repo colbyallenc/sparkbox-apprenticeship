4b8132f73ad578e8fe71853f55e01637c6b55fea
colby allen <sparkuser@sparkusers-MacBook-Air.local>
2019-01-31 09:36:55 -0500

    feat: word frequency exercise this exercise is complete but still a WIP

---
+++ practice/node/word-frequency-finder/script.js
@@ -0,0 +1,33 @@
+'use strict';
+const fs = require('fs');
+
+fs.readFile('word-frequency-finder.txt',  "utf-8", (err,data)=>{
+    
+    if(err){
+        throw err;
+    }
+
+    function wordFreq(string) {
+                                  // " "   splits the array at one single space character.
+                                // /\s/ splits the array at every kind of whitespace character
+                        //         |
+        let words = string.split(/\s/);
+        let freqMap = {};
+
+        // store the # of occurences
+        words.forEach(function(w) {
+            if (!freqMap[w]) {
+                freqMap[w] = 0;
+            }
+            freqMap[w] += 1;
+        });
+        return freqMap;
+    }
+    
+    let freq = wordFreq(data);
+
+    Object.keys(freq).sort().forEach(function(word) {
+        console.log(word + " : " + freq[word]);
+    })
+
+});
