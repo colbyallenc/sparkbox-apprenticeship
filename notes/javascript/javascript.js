e85d3eff701cc9fb88d7e018781ebb13a6aa9f99
colby allen <sparkuser@sparkusers-MacBook-Air.local>
2019-01-31 10:25:24 -0500

    feat: new foundry reading/notes take control of your git history creating good pull request

--- notes/javascript/javascript.js
+++ notes/javascript/javascript.js
@@ -1,1 +1,16 @@
 // first code
+
+// callback:
+// asynchronous -- 
+
+// promises:
+// another way of handling async data/ same concept
+// 
+
+
+
+// *****look up****
+
+// duck typing
+// integration test
+// promises
