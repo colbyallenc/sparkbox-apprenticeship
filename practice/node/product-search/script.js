353df7af1f16a176dc8c5430c00d5c3752cf1056
colby allen <sparkuser@sparkusers-MacBook-Air.local>
2019-02-01 14:14:08 -0500

    docs: removed an unnecesary console.log

--- practice/node/product-search/script.js
+++ practice/node/product-search/script.js
@@ -12,12 +12,11 @@ fs.readFile('product-search.txt',  "utf-8", (err,data)=>{
     // parse the file so that its in JSON
     let parsedData = JSON.parse(data)
     let result = "Not Found."
-    
+
     // iterate through to see which product matches with the product
     for(var i=0; i< parsedData.products.length; i++){
         if(parsedData.products[i].name===product){
                 result = parsedData.products[i]
-                // console.log(parsedData.products.length)
             }  
         }
         console.log(result) 
