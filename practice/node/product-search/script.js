5a9afc53053fc7ef94c70b8bde2204c07b0c38e2
colby allen <sparkuser@sparkusers-MacBook-Air.local>
2019-02-01 14:09:19 -0500

    docs: WOOO!!!! corrections made/ better solution Product-Search solution complete

--- practice/node/product-search/script.js
+++ practice/node/product-search/script.js
@@ -2,29 +2,23 @@
 const fs = require('fs');
 const readlineSync = require('readline-sync');
 
-let product = readlineSync.question('Which product would you like? ');
+const product = readlineSync.question('Which product would you like? ');
 
 // reference the data and look for that product
 fs.readFile('product-search.txt',  "utf-8", (err,data)=>{
     if(err){
         throw err;
     }
-
     // parse the file so that its in JSON
     let parsedData = JSON.parse(data)
- 
+    let result = "Not Found."
+    
     // iterate through to see which product matches with the product
-    for(var i=0; i<= parsedData.products.length; i++){
+    for(var i=0; i< parsedData.products.length; i++){
         if(parsedData.products[i].name===product){
-                console.log(parsedData.products[i])
-            } 
-            console.log("Not Found.")
-    }
-  
+                result = parsedData.products[i]
+                // console.log(parsedData.products.length)
+            }  
+        }
+        console.log(result) 
 });
-
-
-
-
-
-
