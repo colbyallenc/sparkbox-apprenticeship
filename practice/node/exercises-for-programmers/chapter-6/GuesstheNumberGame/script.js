224f57d5ea73abe80e9dc93d17e8bd6ff58261d9
colby allen <sparkuser@sparkusers-MacBook-Air.local>
2019-02-04 16:24:12 -0500

    docs: sudo code and game challenges met I added some sudo code to give a step by step of what im doing I also met challenge requirements

--- practice/node/exercises-for-programmers/chapter-6/GuesstheNumberGame/script.js
+++ practice/node/exercises-for-programmers/chapter-6/GuesstheNumberGame/script.js
@@ -3,8 +3,9 @@ const fs = require('fs');
 const readlineSync = require('readline-sync');
 
 console.log("Lets Play a Game >:) ")
-const difficulty = readlineSync.question('Pick a Level of Difficulty (1, 2, or 3) : ');
 
+// switch case for the type of game the user wants to play
+const difficulty = readlineSync.question('Pick a Level of Difficulty (1, 2, or 3) : ');
 switch (difficulty){
     case "3":
         randomGuess(Math.floor(Math.random() * Math.floor(1000))) 
@@ -19,23 +20,52 @@ switch (difficulty){
 
 
 function randomGuess(randomNum){
-    let numOfGuesses = 1 
+    // game variables
+    let numOfGuesses = 1;
     let correctGuess = false;
+    let madeGuesses = [];
+
+    // loops through the game until correctGuess = true
     while (correctGuess=== false){
+    // prompt the user for a guess an store it 
         const guess = readlineSync.question('I have my number.. What is your guess : ');
+        
+    // if user makes the same guess it tells user they've already guessed that number
+        if(madeGuesses.includes(guess)){
+            console.log("You've already guessed that number!")
+        }
+
+    // tells if guess is too high / too low/ or right on target/ 
+    // prints # of guesses made and counts ++
+    // stores guesses made
         if(guess > randomNum){
             console.log("Too High. Guess Again: " + numOfGuesses)
+            madeGuesses.push(guess);
             numOfGuesses ++
         } else if(guess < randomNum){
             console.log("Too Low. Guess Again: " + numOfGuesses )
+            madeGuesses.push(guess)
             numOfGuesses ++
         } else {
             console.log("You've got it")
             console.log("Number of Guesses  : " + numOfGuesses)
             correctGuess = true
-        }
+        } 
 
     }
 
+    goodGuesser(numOfGuesses)
+}
 
+// tells whether user is a good guesser or not based off # of guesses made
+function goodGuesser(num){
+    if (num>=7){
+        console.log("Better Luck Next time")
+    } else if (num >= 3 && num <= 6){
+        console.log("You can do better than that!")
+    } else if (num >= 2 && num <= 4){
+        console.log("Most Impressive!")
+    } else {
+        console.log("You're a mind reader")
+    }
 }
