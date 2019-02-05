372e457b353e4e88dadbf6c78b3573c8b2b98e23
colby allen <sparkuser@sparkusers-MacBook-Air.local>
2019-02-05 13:21:19 -0500

    docs: Alternative/Possibly better solution

--- practice/node/exercises-for-programmers/chapter-6/multiplication table/script.js
+++ practice/node/exercises-for-programmers/chapter-6/multiplication table/script.js
@@ -4,3 +4,16 @@ for(var i=0; i<=12; i++){
         console.log( i + " x " + j + " = " + product)
     }
 }
+
+
+// Alternative ( Possibly Better) Solution
+
+            // prints out numbers 1-12 as an array
+let table = [...Array(13).keys()];
+
+table.map( i => {
+  table.map( j => {
+    let product = i * j;
+    console.log( i + " x " + j + " = " + product)
+  })
+})
