9ff5ce022060f1b141f0f1342786b1b593709d29
colby allen <sparkuser@sparkusers-MacBook-Air.local>
2019-02-11 16:59:05 -0500

    docs: finally....... my time has come. it works solution complete; however, i know there is still a better way

--- katas/pete-the-baker/pete-the-baker.js
+++ katas/pete-the-baker/pete-the-baker.js
@@ -11,41 +11,38 @@ cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1000, sugar: 1200, eggs: 5, mil
 // cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
 
 // cakes({flour: 500}, {flour: 1000}); 
- 
 
-// function makeCakes(recipe, ingredients) {
-//   return Math.floor(ingredients.flour / recipe.flour);
-// }
+function cakes(recipe, available){
 
-// i created two objects to use as an example
-let obj1 = {sugar: 3, flour: 300, milk: 150};
-let obj2 = {flour: 2000, milk: 2000, sugar: 500};
+  //declare empty arrays for comparison and interger manipulation
+  let recipeArr = []
+  let availableArr = []
+  let howManyCakes = []
 
-//save entries of the objects as arrays
-let recipe = Object.entries(obj1);
-let available = Object.keys(obj2);
-let numArr = [];
+    //push keys of recipe and avaiable into empty arrays
+    for (let key in recipe) {
+      // console.log(`${key} in ${obj1[key]}`)
+      recipeArr.push(key)
+    }
+    for (let key in available) {
+      availableArr.push(key)
+    }
 
-//a nested loop to make access to all of the recipe and avaiable array
-for(var i=0; i <= recipe.length; i++){
-  for(var j=0; j<=available.length; j++){
-    //here i want to compare to see if everything thats in recipe is available 
-    // eeeep... the struggle lies in this vicintity .. all around it. it smells. 
-    console.log(available[j][0] === recipe[i][0]) ? 'true':'false';
+    //check to see if avaiable includes recipe
+    if(!availableArr.includes(...recipeArr)){
+      //if it doesnt, 0 cakes can be made
+      console.log('0')
+      } else {
+        //if everything exists, divide and store numbers
+        for (let val in avaiable) {
+        howManyCakes.push(avaiable[val]/recipe[val])
+      
+      }
+    }
+    //sort the numbers in order to print the lowest number
+    howManyCakes.sort((a,b)=> a-b)
+    //return lowest number in sorted list
+    console.log(howManyCakes[0])
 
-  }
+
 }
-
-//divide object values of recipe and available 
-// store the values
-numArr.push(Math.floor(available[i][1] / recipe[i][1])) //this would work inside of the loop above
-
-//return the smallest value
-  numArr.sort(function(a, b){
-    return a-b}
-    )
-  console.log(numArr[0]);
-
-
-
-// module.exports = makeCakes;
