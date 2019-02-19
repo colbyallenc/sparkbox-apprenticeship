cdabe3fe090b0f7ff30813ab71796b08693f9f7d
colby allen <sparkuser@sparkusers-MacBook-Air.local>
2019-02-19 16:50:36 -0500

    docs(practice): WIP turning madlib into object completely changed my approach turning it into an object

--- practice/exercises-for-programmers/chapter-2/madLib/script.js
+++ practice/exercises-for-programmers/chapter-2/madLib/script.js
@@ -1,4 +1,10 @@
+const readline= require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
+<<<<<<< Updated upstream
 'use strict';
 const fs = require('fs');
 const readlineSync = require('readline-sync');
@@ -17,3 +23,44 @@ const adverbUsage = readlineSync.question(`Does ${noun} ${verb} ${adverb} ? `)
 console.log(`So youre telling me that ${noun} is the ${adverb}est ${verb}er you know? Woah there.`)
 const nice = readlineSync.question(`Is ${noun} a nice person at least? `)
 // console.log(nice)
+=======
+<<<<<<< Updated upstream
+=======
+
+class MadLib {
+    constructor(){
+        this.noun;
+        this.verb;
+        this.adverb;
+    }
+
+    getMadlib(){
+        console.log(`${this.noun} ${this.verb} ${this.adverb}`)
+    }
+
+    collectWords(){
+        rl.question('Enter a noun : ', (noun) => {
+            this.noun = noun
+            rl.question('Enter a verb : ', (verb)=>{
+                this.verb = verb
+                rl.question('Enter a adverb: ', (adverb) => {
+                    this.adverb = adverb
+                    rl.close();
+                });
+            });
+        });
+
+        getMadlib()
+    
+
+    }
+
+   
+}
+
+let newMadLib = new MadLib();
+newMadLib.collectWords();
+
+// newMadLib.getMadlib();
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
