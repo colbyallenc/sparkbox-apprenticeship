abeb2f38bcb19326d6971713378c18d9c33d7016
colby allen <colbyallenc@gmail.com>
2019-03-06 18:40:11 -0500

    feat(katas):beginning two sum solution code from codewars

---
+++ katas/two_Sum/script.js
@@ -0,0 +1,14 @@
+let twoSum = function(nums, target){
+    let indices = [];
+    let indecesObj = {};
+    for(let i = 0 ; i < nums.length; i++){
+        for(let j = 0 ; j < nums.length; j++){
+            if(nums[i] + nums[j] == target){
+                indecesObj.push(num[i][j])
+            }                      
+            
+        }
+    }   
+}
+
+twoSum([2, 7, 8, 1], 9)
